12358ec1860b6b92642f48d54a8e8ba06d264e0a
Juan Fernandez <jfernan1@ualberta.ca>
2025-11-26 16:59:47 -0700

    Fourier gauss

--- noises/fourier/fourier_filter.py
+++ noises/fourier/fourier_filter.py
@@ -5,18 +5,17 @@ def fourier_denoise(image, percentile=85, K=50):
     # 1. FFT
     F = fftpack.fft2(image)
     F_shift = fftpack.fftshift(F)  # center DC
-    M, N = image.shape
+    H, W = image.shape
 
     # 2. Magnitude of centered spectrum
     mag = np.abs(F_shift)
 
     # 3. Optionally zero a small DC block (low frequencies)
-    mag[M//2 - K:M//2 + K, N//2 - K:N//2 + K] = 0
+    mag[H//2 - K:H//2 + K, W//2 - K:W//2 + K] = 0
 
     # 4. Keep coefficients below some percentile (remove large peaks)
     thresh = np.percentile(mag, percentile)
-    mask = mag < thresh  # True for small coefficients to keep
-
+    mask = mag < thresh  # Keep the small coefficients as we only care about the noise right now, however we can't remove too many small coefficients.
     # 5. Apply mask in centered domain
     F_shift_filtered = F_shift * mask
 
