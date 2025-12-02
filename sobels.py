2a9f3c35f922312141ec3639ef98461c450b1fda
alexanderneva <amshulha@ualberta.ca>
2025-12-02 16:52:05 -0700

    updated sobel and thresholding figures

--- sobels.py
+++ sobels.py
@@ -1,9 +1,11 @@
 from Utils.load_utils import load_bsds500
 from noise import add_gaussian_noise
 from matplotlib import pyplot as plt
+from sklearn.metrics import mean_squared_error
 from metrics import calculate_accuracy, psnr, mcc
 from wavelet_denoising_example import dwt2_denoise, multilevel_denoise
 from canny import canny
+import numpy as np
 import cv2
 import pywt
 
@@ -20,28 +22,46 @@ def sobels():
     # flowers - 22!
     # shoes - 32
     # others - 28, 30, 35, 41
-
+    
     #eagle photo 
-    image = images[21]
-    sobels = []
-    plt.imshow(image)
-    plt.savefig("grayeagle.jpg",bbox_inches='tight',dpi=200)
-    plt.title("Grayscale Eagle")
+    image = images[20]
+    noised_image = add_gaussian_noise(image, 20, 40, 1)
+    fig, (ax1,ax2) = plt.subplots(1,2)
+    ax1.imshow(image, cmap = 'gray')
+    ax1.set_title('Original from BSDS')
+    ax2.imshow(noised_image,cmap = 'gray')
+    ax2.set_title('Noised Image mu = 20, var = 40, gamma=1')
+    plt.savefig('noisecomparison.jpg', bbox_inches='tight',dpi=200)
     plt.show()
+#    colour_image = cv2.imread('coloureagle.jpg')
+
+#    sobels = []
+#    fig, (ax1,ax2) = plt.subplots(1,2)
+#    ax1.imshow(colour_image)
+#    ax1.set_title('Colourized Eagle')
+#    ax2.imshow(image)
+#    ax2.set_title('Grayscale Eagle')
+#    plt.savefig("grayeagle.jpg",bbox_inches='tight',dpi=200)
+#    plt.show()
     ## iterating through kernel size
+    sobels = []
+    sobels_denoise = []
     for i in [1,3,5,7]:
-        test = cv2.Sobel(image,-1, dx=1,dy=0,ksize = i)
-        sobels.append(test)
+        test1 = cv2.Sobel(image,-1, dx=1,dy=0,ksize = i)
+        test2 = cv2.Sobel(image, -1, dx=1,dy=0,ksize= i)
+        #abs_sobel64f = np.absolute(test)
+        #test = np.uint8(abs_sobel64f)
+        sobels.append(test1)
 
 
     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
-    ax1.imshow(sobels[0])
+    ax1.imshow(sobels[0], cmap="gray")
     ax1.set_title("Sobel Kernel Size 1 x 1")
-    ax2.imshow(sobels[1])
+    ax2.imshow(sobels[1], cmap="gray")
     ax2.set_title("Sobel Kernel Size 3 x 3")
-    ax3.imshow(sobels[2])
+    ax3.imshow(sobels[2], cmap="gray")
     ax3.set_title("Sobel Kernel Size 5 x 5")
-    ax4.imshow(sobels[3])
+    ax4.imshow(sobels[3], cmap="gray")
     ax4.set_title("Sobel Kernel Size 7 x 7")
     # ax1.imshow(image, cmap='gray')
     # ax2.imshow(noisy_image, cmap='gray')
@@ -50,6 +70,42 @@ def sobels():
     plt.savefig("sobel_comparison.jpg", bbox_inches='tight', dpi=200)
     plt.show()
 
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+    ax1.imshow(sobels[0], cmap="gray")
+    ax1.set_title("Sobel Kernel Size 1 x 1")
+    ax2.imshow(sobels[1], cmap="gray")
+    ax2.set_title("Sobel Kernel Size 3 x 3")
+    ax3.imshow(sobels[2], cmap="gray")
+    ax3.set_title("Sobel Kernel Size 5 x 5")
+    ax4.imshow(sobels[3], cmap="gray")
+    ax4.set_title("Sobel Kernel Size 7 x 7")
+    plt.savefig("sobel_noise_comparison.jpg",bbox_inches='tight',dpi=200)
+    plt.show()
+
+    
+    haars = []
+    haars_denoise = []
+    for j in ["soft","hard","greater"]:
+        image2 = dwt2_denoise(image, 'haar', j, 20)
+        image3 = dwt2_denoise(noised_image, 'haar', j, 30)
+        haars.append(image2)
+        haars_denoise.append(image3)
+
+    fig, ((ax1,ax2,ax3),(ax4,ax5,ax6)) = plt.subplots(2,3)
+    ax1.imshow(haars[0],cmap='gray')
+    ax1.set_title('Soft Thresholding C = 30')
+    ax2.imshow(haars[1],cmap='gray')
+    ax2.set_title('Hard Thresholding C = 30')
+    ax3.imshow(haars[2],cmap='gray')
+    ax3.set_title('Greater C = 30')
+    ax4.imshow(haars_denoise[0], cmap = 'gray')
+    ax4.set_title('Soft C = 30 on noised image')
+    ax5.imshow(haars_denoise[1], cmap = 'gray')
+    ax5.set_title('Hard C = 30 on noised image')
+    ax6.imshow(haars_denoise[2], cmap = 'gray')
+    ax6.set_title('Greater C =30 on noised image')
+    plt.show()
+    plt.savefig('thresholdingcomparison.jpg',bbox_inches='tight',dpi=200)
     # orthogonal: coif, db, haar, sym
 
 
