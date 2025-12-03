9274b4d29d46f3131c6799f95616250cacf7bb6b
Jacob Garber <jgarber1@ualberta.ca>
2025-12-03 16:23:15 -0700

    numpy noise

--- noise.py
+++ noise.py
@@ -1,10 +1,9 @@
 import numpy as np
 import cv2
 
-def add_gaussian_noise(image, mean=0.0, stddev=15.0, gamma=1.0):
+def add_gaussian_noise(rng, image, mean, stddev, gamma):
     img = image.astype(np.float32)
-    noise = np.zeros_like(img, dtype=np.float32)
-    cv2.randn(noise, mean, stddev)
+    noise = rng.normal(mean, stddev, image.shape)
     img_noisy = img + gamma * noise
     img_noisy = np.clip(img_noisy, 0, 255)
     return img_noisy.astype(np.uint8)
